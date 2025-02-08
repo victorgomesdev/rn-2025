58011b5c5e05ed78a253987ea4676611d4b9484d
Victor Gomes <104176261+victorgomesdev@users.noreply.github.com>
2025-02-08 00:25:57 +0000

    07/02/2025

---
+++ aula00-JS-Revisão/exemplo1.js
@@ -0,0 +1,28 @@
+// Exemplos de variáveis
+
+// Usando *var*
+// Escopo global
+
+// var nome = "Victor"
+
+// if(true){
+//     var nome = "Maria"
+
+//     console.log(nome) // Maria
+// }
+
+// console.log(nome)
+
+//###############################
+// Usando *let*
+
+// let nome = "Victor"
+
+// if(true){
+//     let nome ="Maria"
+    
+//     console.log(nome)
+
+// }
+
+// console.log(nome)
